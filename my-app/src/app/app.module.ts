2e77ddb1f640c0d311f93bfdb11048af48636743
siavashasachi <asachiciavash@gmail.com>
2023-05-08 12:24:52 +0430

    change body's background color

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './header/navigation/navigation.component';
+import { ContentComponent } from './content/content.component';
+import { SliderComponent } from './content/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    ContentComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
