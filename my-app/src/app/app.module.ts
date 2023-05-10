64f6cfe4bf8bc51b7a12ffb2ac3e38f6289324f3
siavashasachi <asachiciavash@gmail.com>
2023-05-10 12:53:01 +0430

    create image slider with ng slider,but we need some changes in future for styles

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,14 +7,20 @@ import { NavigationComponent } from './header/navigation/navigation.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { NgImageSliderModule } from 'ng-image-slider';
 import { SlidesComponent } from './slides/slides.component';
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavigationComponent, SlidesComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    SlidesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SlickCarouselModule,
     NgImageSliderModule,
-
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
--- my-app/src/app/slides/slides.component.ts
+++ my-app/src/app/slides/slides.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class SlidesComponent {
 
+  imageObject: Array<object> = [{
+    image: '../assets/images/slider-images/1.jpg',
+    thumbImage: '../assets/images/slider-images/1.jpg',
+    alt: 'alt of image',
+    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید!',
+    id:1
+  }, {
+    image: '../assets/images/slider-images/2.jpg',
+    thumbImage: '../assets/images/slider-images/1.jpg',
+    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید!',
+    alt: 'Image alt'
+  }, {
+    image: '../assets/images/slider-images/3.jpg',
+    thumbImage: '../assets/images/slider-images/1.jpg',
+    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید!',
+    alt: 'Image alt'
+  }
+  ];
 }
