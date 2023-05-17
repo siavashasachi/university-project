b2d5b37912e08016c33d027666feab8bf62f1b62
siavashasachi <asachiciavash@gmail.com>
2023-05-17 10:18:17 +0430

    Add FAde on Scrolling

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'دوچرخه-اصفهان';
-
-
+  ngOnInit() {
+    AOS.init();
+  }
 }
