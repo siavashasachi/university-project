8b1a105c40479a3f217031e91614db82fb8ab399
siavashasachi <asachiciavash@gmail.com>
2023-05-18 13:05:05 +0430

    create settimeout on successhelp but it need a lot of changes yet

--- my-app/src/app/success-help/success-help.component.ts
+++ my-app/src/app/success-help/success-help.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 export class SuccessHelpComponent {
  progress:string='درحال اجرا';
  sendToSuporter:string='درحال ارسال اطلاعات به پشتیبانی';
-
+  settimeout=setTimeout(() => {
+  this.progress='با موفقیت انجام شد';
+  this.sendToSuporter="جهت اطلاعات تکمیلی با شما تماس گرفته خواهد شد";
+}, 3000 )
  constructor(){
 
  }
 cansel(){
 process.exit()}//it causes to doesn't send information to server
-// showSucess(): any = setTimeout(() => {
-//   this.progress='ّبا موفقیت انجام شد';
-//   this.sendToSuporter="جهت اطلاعات تکمیلی با شما تماس گرفته خواهد شد";
-// }, 5000);
+
 }
