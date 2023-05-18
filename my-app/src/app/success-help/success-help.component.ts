d5b964c2026eb1ef05e69e8dcf1b71d5c999dd2e
siavashasachi <asachiciavash@gmail.com>
2023-05-18 13:27:13 +0430

    rewrite methods of success help but we need a little changes

--- my-app/src/app/success-help/success-help.component.ts
+++ my-app/src/app/success-help/success-help.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
+import { run } from 'node:test';
 @Component({
   selector: 'app-success-help',
   templateUrl: './success-help.component.html',
   styleUrls: ['./success-help.component.css'],
 })
 export class SuccessHelpComponent {
- progress:string='درحال اجرا';
- sendToSuporter:string='درحال ارسال اطلاعات به پشتیبانی';
-  settimeout=setTimeout(() => {
-  this.progress='با موفقیت انجام شد';
-  this.sendToSuporter="جهت اطلاعات تکمیلی با شما تماس گرفته خواهد شد";
-}, 3000 )
- constructor(){
-
- }
-cansel(){
-process.exit()}//it causes to doesn't send information to server
-
+  progress: string = 'درحال اجرا';
+  sendToSuporter: string = 'درحال ارسال اطلاعات به پشتیبانی';
+  btnsubject: string = 'لغو';
+  cansel() {
+    if (this.btnsubject === 'بازگشت') {
+      //it causes to disable cansel method
+    } else process.exit();
+  } //it causes to doesn't send information to server
+  settimeout = setTimeout(() => {
+    this.progress = 'با موفقیت انجام شد';
+    this.sendToSuporter = 'جهت اطلاعات تکمیلی با شما تماس گرفته خواهد شد';
+    this.btnsubject = 'بازگشت';
+  }, 3000); //it changes information after 3 seconds
+  constructor() {}
 }
