70b0545fbdeb5b5e70c833638ca1bb6125b08dac
siavashasachi <asachiciavash@gmail.com>
2023-05-18 14:52:26 +0430

    start to create progressbar

--- my-app/src/app/success-help/success-help.component.ts
+++ my-app/src/app/success-help/success-help.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import { disableDebugTools } from '@angular/platform-browser';
-import { run } from 'node:test';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-success-help',
   templateUrl: './success-help.component.html',
   styleUrls: ['./success-help.component.css'],
 })
-export class SuccessHelpComponent {
+export class SuccessHelpComponent implements OnInit {
   progress: string = 'درحال اجرا';
   sendToSuporter: string = 'درحال ارسال اطلاعات به پشتیبانی';
   btnsubject: string = 'لغو';
+
   cansel() {
     if (this.btnsubject === 'بازگشت') {
       //it causes to disable cansel method
@@ -21,4 +20,6 @@ export class SuccessHelpComponent {
     this.btnsubject = 'بازگشت';
   }, 3000); //it changes information after 3 seconds
   constructor() {}
+  ngOnInit() {
+  }
 }
