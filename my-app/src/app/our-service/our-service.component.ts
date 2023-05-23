55b5fb17ef8cf421f66a1f71654a037184e307af
siavashasachi <asachiciavash@gmail.com>
2023-05-23 11:17:21 +0330

    routing for ourservice

--- my-app/src/app/our-service/our-service.component.ts
+++ my-app/src/app/our-service/our-service.component.ts
@@ -11,18 +11,21 @@ export class OurServiceComponent {
       src: '../assets/images/ourservice/place.jpg',
       alt: 'this is place for renting bycicles',
       header: 'اجاره دادن مکان نگه داری ',
-      title:'اجاره انبار برای منطقه های پیشنهادی شهرداری'    },
+      title:'اجاره انبار برای منطقه های پیشنهادی شهرداری' ,
+    route:'/rentplace'   },
     {
       src: '../assets/images/ourservice/bycicle.jpg',
       alt: 'this is a bycicle',
       header: 'اجاره دادن دوچرخه',
       title: 'اجاره دادن دوچرخه مناسب به شهرداری ',
+      route:'/rentbicycle'
     },
     {
       src: '../assets/images/ourservice/repair.jpg',
       alt: 'it repairs bycicle',
       header: 'سرویس دوچرخه',
       title: 'سرویس دوچرخه در نزدیکی انبارهای دوچرخه',
+      route:'/serviceb'
     },
   ];
 }
