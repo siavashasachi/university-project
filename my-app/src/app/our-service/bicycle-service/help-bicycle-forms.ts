b0ae5c14685c6a611df0564315e91f245619d695
siavashasachi <asachiciavash@gmail.com>
2023-05-23 14:33:11 +0330

    some changes

---
+++ my-app/src/app/our-service/bicycle-service/help-bicycle-forms.ts
@@ -0,0 +1,12 @@
+
+export class HelpBicycleForms {
+
+    constructor(
+      public  identify:number, 
+      public  phoneNumber:number,
+      public  name:string,
+      public  lastName:string,
+
+    ) {  }
+  
+}
