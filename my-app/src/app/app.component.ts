a13298441ff1b3af4d14070d64e6708b39524f0a
siavashasachi <asachiciavash@gmail.com>
2023-05-23 10:59:15 +0330

    add loading bar and error message for routing

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
-import { ProgressBarMode } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +7,16 @@ import { ProgressBarMode } from '@angular/material/progress-bar';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  
   title = 'دوچرخه-اصفهان';
-  mymode: ProgressBarMode = 'query';
-  loader: any;
-  ngOnInit() {
-    AOS.init({disable: 'mobile'});//AOS - 2
-    AOS.refresh();//refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
-  }
 
+  ngOnInit() {
+    AOS.init({ disable: 'mobile' }); //AOS - 2
+    AOS.refresh(); //refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
+
+  }
+  constructor() {
+
+  }
+  
 }
--- my-app/src/app/header/navigation/navigation.component.ts
+++ my-app/src/app/header/navigation/navigation.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router, RouterOutlet, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent  implements OnInit{
+  myvalue: number=0;
+  mycolor:string='#fff';
+  ErrorMessageNotFound:string='';
+  ErrorDisplay:string='';
+  ngOnInit() { this.router.events.subscribe((event) => {
+    if (event instanceof NavigationStart) {
+      // Navigation is starting... show a loading spinner perhaps?
+      this.myvalue=30;
+      this.mycolor='#fff';//white
+      this.ErrorDisplay='none';
+    }
+    if (event instanceof NavigationEnd) {
+      // We've finished navigating
+      this.myvalue=50;
+      this.mycolor='rgb(0, 255, 34)';//green
+      setTimeout(() => {
+        this.myvalue=100;
+      }, 1500);
+      setTimeout(() => {
+        this.myvalue=0;
+      }, 2000);
+    }
+    if (event instanceof NavigationError) {
+      // something went wrong, log the error
+      this.myvalue=100;
+      this.mycolor='rgb(206, 34, 34)';//red
+      this.ErrorMessageNotFound='صفحه مورد نظر یافت نشد با استفاده از منوی بالا به صفحه مورد نظر بازگردید';
+      this.ErrorDisplay='display:block;'
+    }
+  });
+}
+constructor(private router: Router) {
 
 }
+}
+
+
