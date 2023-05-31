ab3cd8577fcad4f68875ec5edbf2238da6dfce75
siavashasachi <asachiciavash@gmail.com>
2023-05-31 12:38:32 +0330

    change on styles

--- my-app/src/app/our-service/bicycle-service/bicycle-service.component.ts
+++ my-app/src/app/our-service/bicycle-service/bicycle-service.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { MaxLengthValidator, MinLengthValidator } from '@angular/forms';
 @Component({
   selector: 'app-bicycle-service',
   templateUrl: './bicycle-service.component.html',
   styleUrls: ['./bicycle-service.component.css'],
 })
 export class BicycleServiceComponent {
-  name = new FormControl('');
-  identifyCode = new FormControl(Number);
- lastName=new FormControl('');
- phoneNumber=new FormControl(Number);
+  @Input()
+  appInputMaxLength!: number;
+  myBorder:string='';
+
   constructor(){}
 }
