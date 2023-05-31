f513214568370add87c791486574d9a28bc91657
siavashasachi <asachiciavash@gmail.com>
2023-05-31 13:37:20 +0330

    change on styles

--- my-app/src/app/slides/slides.component.ts
+++ my-app/src/app/slides/slides.component.ts
@@ -11,17 +11,17 @@ export class SlidesComponent {
     image: '../assets/images/slider-images/1.jpg',
     thumbImage: '../assets/images/slider-images/1.jpg',
     alt: 'alt of image',
-    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید!',
+    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید',
     id:1
   }, {
     image: '../assets/images/slider-images/2.jpg',
     thumbImage: '../assets/images/slider-images/1.jpg',
-    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید!',
+    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید',
     alt: 'Image alt'
   }, {
     image: '../assets/images/slider-images/3.jpg',
     thumbImage: '../assets/images/slider-images/1.jpg',
-    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید!',
+    title: 'با دوچرخه به مقصد برسید و از مسیر لذت ببرید',
     alt: 'Image alt'
   }
   ];
