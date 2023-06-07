630bc29ccfd0f0bc462ab194ed13d41264a68566
siavashasachi <asachiciavash@gmail.com>
2023-06-07 10:18:24 +0330

    add some error controller

--- my-app/src/app/our-service/bicycle-service/bicycle-service.component.ts
+++ my-app/src/app/our-service/bicycle-service/bicycle-service.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from '@angular/core';
-import { MaxLengthValidator, MinLengthValidator } from '@angular/forms';
 @Component({
   selector: 'app-bicycle-service',
   templateUrl: './bicycle-service.component.html',
   styleUrls: ['./bicycle-service.component.css'],
 })
 export class BicycleServiceComponent {
-  @Input()
-  appInputMaxLength!: number;
-  myBorder:string='';
-
-  constructor(){}
+  ErrorMessage = '';
+  ErrorControler='';
+  constructor() {}
+  isValid(event: Event) {
+    if ((<HTMLInputElement>event.target).value.length > 10) {
+      this.ErrorMessage = 'کد ملی شما بیشتر از حد مجاز است';
+      this.ErrorControler=`color:var(--error-message);`;
+    } else if ((<HTMLInputElement>event.target).value.length < 10) {
+      this.ErrorMessage = 'کد ملی شما کمتر از حد مجاز است';
+      this.ErrorControler=`color:var(--error-message);`;
+    } else if ((<HTMLInputElement>event.target).value.length === 10) {
+      this.ErrorMessage = 'دارای اعتبار';
+      this.ErrorControler=`color:var(--btn-success);`;
+    }
+  }
 }
