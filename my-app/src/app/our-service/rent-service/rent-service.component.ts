c9f499316ee653241e6c93adeb7a47d49cfdb761
siavashasachi <asachiciavash@gmail.com>
2023-06-22 10:14:32 +0330

    copy forms to edit in the future

--- my-app/src/app/our-service/rent-service/rent-service.component.ts
+++ my-app/src/app/our-service/rent-service/rent-service.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./rent-service.component.css']
 })
 export class RentServiceComponent {
+  ErrorMessage = '';
+  ErrorNumber='';
+  ErrorControler='';
+  ErrorControlerN='';
+  phoneNumber:number=913;
+  constructor() {
+    if (this.phoneNumber.toLocaleString().length> 11) {
+      this.ErrorNumber = 'شماره تلفن  شما بیشتر از حد مجاز است';
+      this.ErrorControlerN=`color:var(--error-message);`;
+    } else if (this.phoneNumber < 11) {
+      this.ErrorNumber = 'شماره تلفن شما کمتر از حد مجاز است';
+      this.ErrorControlerN=`color:var(--error-message);`;
+    } else if (this.phoneNumber=== 11) {
+      this.ErrorNumber= 'دارای اعتبار';
+      this.ErrorControlerN=`color:var(--btn-success);`;
+    }
+  }
+  isValid(event: Event) {
+    if ((<HTMLInputElement>event.target).value.length > 10) {
+      this.ErrorMessage = 'کد ملی شما بیشتر از حد مجاز است';
+      this.ErrorControler=`color:var(--error-message);`;
+    } else if ((<HTMLInputElement>event.target).value.length < 10) {
+      this.ErrorMessage = 'کد ملی شما کمتر از حد مجاز است';
+      this.ErrorControler=`color:var(--error-message);`;
+    } else if ((<HTMLInputElement>event.target).value.length === 10) {
+      this.ErrorMessage = 'دارای اعتبار';
+      this.ErrorControler=`color:var(--btn-success);`;
+    }
+  }
+  
 
 }
